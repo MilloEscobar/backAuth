f179df118e64ff613a6ea1541d64c4c52156fa10
Jaime Escobar <jescobar@konradgroup.com>
2018-03-01 09:37:26 -0600

    mdb

--- src/config/config.js
+++ src/config/config.js
@@ -4,7 +4,7 @@ var rootPath = path.normalize(__dirname + '/../../');
 module.exports = {
     development: {
         rootPath: rootPath,
-        db: 'mongodb://localhost/customers',
+        db: 'mongodb://mbAnmin:12qw12qw@ds247688.mlab.com:47688/microbible',
         port: process.env.PORT || 3000,
         secret: "56950fe494af8e88204adf6d"
     }
