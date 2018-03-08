eafe29a6adddf52e1e45dddde969c4e02b230073
Jaime Escobar <jescobar@konradgroup.com>
2018-03-08 16:57:59 -0600

    update

--- src/controllers/productsControllers/updateProduct.js
+++ src/controllers/productsControllers/updateProduct.js
@@ -2,17 +2,12 @@ var productData = require('../../data/productData');
 
 module.exports = function updateProduct(req, res, next) {
 	// if (req.user && req.user.roles.indexOf('admin') > -1) {
-	  productData.updateProduct({id: req.body.id}, req.body, function (err, product) {
-		  if (err) {
-		    res.send( {status : 'error', message: 'Product Update failed'});
-		  }
-		  if (!product) {
-		  	res.send({status : 'error', message: 'Product Not Found'});
-		  }else{
-		  	res.send( {status : 'success', message: 'Product update success'});
-		  }
-		  
-	  });
+	productData.updateProduct({id: req.body.id}, req.body, function (err, product) {
+		if (err) {return res.send( {status : 'error', message: 'Product Update failed'});}
+		if (product["n"] == 0) {return res.send({status : 'error', message: 'Product Not Found'});}
+		if (product["nModified"] == 0) {return res.send({status : 'success', message: 'Product found but no fields were modified'});}
+		res.send( {status : 'success', message: 'Product updated successfuly'});
+	});
 	// }else{
 		// res.send( {status : 'error', message: 'You dont have permissions'});
 	// }
